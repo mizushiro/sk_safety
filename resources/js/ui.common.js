42db8312df2e104c3d6953cd47ac2d18232ba6d3
asever77 <johyunmin77@gmail.com>
2021-06-13 12:33:23 +0900

    d

--- resources/js/ui.common.js
+++ resources/js/ui.common.js
@@ -4,29 +4,45 @@
 	
 	$plugins.common = {
 		init: function(){
-
+			//hover event
 			$(document).on("touchstart", function(){ });
-			
-			$plugins.uiAjax({ 
-				id: $('.base-header'), 
-				url:'../inc/header.html', 
-				page:true, 
-				callback:$plugins.common.header 
-			});
 
-			$plugins.uiAjax({ 
-				id: $('.base-footer'), 
-				url:'../inc/footer.html', 
-				page:true, 
-				callback:$plugins.common.footer 
-			});
+			//최소높이 설정
+			$(win).on('resize', $plugins.common.pageMinHeight);
+			this.pageMinHeight();
+
+			if ($('.base-header').length) {
+				$plugins.uiAjax({ 
+					id: $('.base-header'), 
+					url:'../inc/header.html', 
+					page:true, 
+					callback:$plugins.common.header 
+				});
+			}
+			if ($('.base-footer').length) {
+				$plugins.uiAjax({ 
+					id: $('.base-footer'), 
+					url:'../inc/footer.html', 
+					page:true, 
+					callback:$plugins.common.footer 
+				});
+			}	
 
 			$plugins.uiInputClear();
 			
 
 		},
+		pageMinHeight: function(){
+			var hh =  $('.base-header').length ? $('.base-header').outerHeight() : 0;
+			var th =  $('.tit-page').length ? $('.tit-page').outerHeight() : 0;
+			var min_h = $(win).outerHeight() - hh - th;
+
+			$('.ui-minheight').css('min-height',min_h + 'px');
+		},
 		header: function(){
-			console.log($('.base-header').data('pageid'));
+			var tit  = $('.base-header').data('title');
+			
+			$('.base-header-wrap .page-title').html(tit);
 		},
 		navOpen: function(v){
 			
